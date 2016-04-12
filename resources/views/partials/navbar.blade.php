216a75a70bbb92547c052e37d27237ad6831b34c
maiklez <maik.ledzep@gmail.com>
2016-04-12 17:32:30 +0200

    without home view

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,11 +17,11 @@
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
+                <!-- Left Side Of Navbar 
                 <ul class="nav navbar-nav">
                     <li><a href="{{ url('/home') }}">Home</a></li>
                 </ul>
-                
+                -->
                 @can('admin', Auth::user())
     			<!-- The Current User Can Update The Post -->
 
