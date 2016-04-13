1d18ee1677ac24585bf4df437235d97a2b4174ee
maiklez <maik.ledzep@gmail.com>
2016-04-13 16:30:04 +0200

    comments on the post

--- resources/views/one_post.blade.php
+++ resources/views/one_post.blade.php
@@ -13,6 +13,9 @@ MaikBlog -
     	<div class="row">
         <div class="panel panel-post">
             
+            @if(!Auth::guest() && ($post->author_id == Auth::user()->id || Auth::user()->is_admin()))
+				<button class="btn" style="float: right"><a href="{{ url('post/'.$post->id .'/edit')}}">Edit Post</a></button>
+			@endif
 
             <div class="panel-body">
                  
@@ -21,8 +24,48 @@ MaikBlog -
 					<div class="post-published">Published by {{ $post->author->name }} on {{ $post->created_at->format('M d,Y') }}</div>
 					<div class="post-body">{!!  $post->body !!}</div>
 					
-					            
-                 
+					 
+					 <!-- The Comments -->
+					 <div style="margin-top:40px;">
+						<h2>Deja tu comentario!</h2>
+					</div>           
+                 	
+                 	@if(Auth::guest())
+						<p>Login to Comment</p>
+					@else
+						<div class="panel-body">
+							<form method="post" action="{!! route('add_comment', $post->id) !!}">
+								<input type="hidden" name="_token" value="{{ csrf_token() }}">
+								<input type="hidden" name="on_post" value="{{ $post->id }}">
+								<input type="hidden" name="slug" value="{{ $post->slug }}">
+								<div class="form-group">
+									<textarea required="required" placeholder="Enter comment here" name = "body" class="form-control"></textarea>
+								</div>
+								<input type="submit" name='post_comment' class="btn btn-success" value = "Post"/>
+							</form>
+						</div>
+					@endif
+					
+					<div>
+						@if(isset($comments))
+						<ul style="list-style: none; padding: 0">
+							@foreach($comments as $comment)
+								<li class="panel-body">
+									<div class="list-group">
+										<div class="list-group-item">
+											<h3>{{ $comment->author->name }}</h3>
+											<p>{{ $comment->created_at->format('M d,Y \a\t h:i a') }}</p>
+										</div>
+										<div class="list-group-item">
+											<p>{{ $comment->body }}</p>
+										</div>
+									</div>
+								</li>
+							@endforeach
+						</ul>
+						@endif
+					</div>      
+                 	<!-- end comments -->
             </div>
         </div>
         </div>
